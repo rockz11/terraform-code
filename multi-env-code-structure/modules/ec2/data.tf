1528b582090358ebc27378d02ffc5f3284d9c75f
Prughvi <prudhviraj3525@gmail.com>
2024-12-19 16:00:12 +0530

    m

--- multi-env-code-structure/modules/ec2/data.tf
+++ multi-env-code-structure/modules/ec2/data.tf
@@ -1,5 +1,5 @@
 
-data "aws_ami" "ami" {
+data "aws_ami" "sg" {
   most_recent = true
   name_regex  = "RHEL-DevOps-Practice"
   owners = ["973714476881"]
--- multi-env-code-structure/modules/ec2/main.tf
+++ multi-env-code-structure/modules/ec2/main.tf
@@ -26,7 +26,7 @@ resource "aws_security_group" "sg" {
 
 
 
-resource "aws_instance" "ami" {
+resource "aws_instance" "instance" {
   ami           = data.aws_ami.ami.id
   instance_type = var.instance_type
   vpc_security_group_ids = [aws_security_group.sg.id]
