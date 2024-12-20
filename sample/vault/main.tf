f0ba39d255809aa799e5e6e8ffd62cefff40f09b
Prughvi <prudhviraj3525@gmail.com>
2024-12-20 19:17:11 +0530

    n

--- sample/vault/main.tf
+++ sample/vault/main.tf
@@ -1,30 +1,45 @@
-terraform {
-  required_providers {
-    vault = {
-      source  = "hashicorp/vault"
-      version = "4.3.0"
-    }
-    local = {
-      source  = "hashicorp/local"
-      version = "2.5.1"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     vault = {
+#       source  = "hashicorp/vault"
+#       version = "4.3.0"
+#     }
+#     local = {
+#       source  = "hashicorp/local"
+#       version = "2.5.1"
+#     }
+#   }
+# }
+# provider "vault" {
+#   address = "https:172.31.87.131:8200"
+#   token = var.vault_token
+# }
+# variable "vault_token" {}
+#
+# data "vault_kv_secret_v2" "example" {
+#   name  = "my_credentials"
+#   mount = "test"
+# }
+# # resource "local_file" "foo" {
+# #   content = data.vault_kv_secret_v2.example.data_json
+# #   filename = "/tmp/secret"
+# # }
+#
+# # output "json" {
+# #   value = data.vault_kv_secret_v2.example.data_json
+# }
+
 provider "vault" {
-  address = "https:172.31.87.131:8200"
+  address = "https://172.31.87.131:8200"
   token = var.vault_token
 }
 variable "vault_token" {}
 
 data "vault_kv_secret_v2" "example" {
-  name  = "my_credentials"
   mount = "test"
+  name  = "my_credentials"
 }
-# resource "local_file" "foo" {
-#   content = data.vault_kv_secret_v2.example.data_json
-#   filename = "/tmp/secret"
-# }
-
-# output "json" {
-#   value = data.vault_kv_secret_v2.example.data_json
+resource "local_file" "foo" {
+  content = jsonencode(data.vault_kv_secret_v2.data.json)
+  filename = "/tmp/secret"
 }
