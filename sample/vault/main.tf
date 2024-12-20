509d7ced33332dd134c8e307b7763f94c8c9c402
Prughvi <prudhviraj3525@gmail.com>
2024-12-20 18:01:18 +0530

    n

--- sample/vault/main.tf
+++ sample/vault/main.tf
@@ -20,7 +20,11 @@ data "vault_kv_secret_v2" "example" {
   name  = "my_credentials"
   mount = "test"
 }
-resource "local_file" "foo" {
-  content = data.vault_kv_secret_v2.example
-  filename = "/tmp/secret"
+# resource "local_file" "foo" {
+#   content = data.vault_kv_secret_v2.example.data_json
+#   filename = "/tmp/secret"
+# }
+
+output "json" {
+  value = data.vault_kv_secret_v2.example.data_json
 }
