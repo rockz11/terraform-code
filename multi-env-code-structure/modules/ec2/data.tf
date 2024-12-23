17a59d15055cbe46ec2391a7fbe81195f36f32ff
Prughvi <prudhviraj3525@gmail.com>
2024-12-23 21:58:25 +0530

    m

--- multi-env-code-structure/modules/ec2/data.tf
+++ multi-env-code-structure/modules/ec2/data.tf
@@ -1,6 +1,21 @@
-
+terraform {
+  required_providers {
+    vault = {
+      source  = "hashicorp/vault"
+      version = "4.3.0"
+    }
+  }
+}
 data "aws_ami" "ami" {
   most_recent = true
   name_regex  = "RHEL-9-DevOps-Practice"
   owners      = ["973714476881"]
+}
+# data "aws_ami" "ami" {
+#   most_recent = true
+#   name_regex  = "RHEL-golden-image"
+#   owners      = ["self"]
+# }
+data "vault_generic_secret" "ssh" {
+  path = "infra-secrets/ssh"
 }
